58e6928e23f39e4dde76128fb6b8b9ae7556cfcc
pyf <498387192@qq.com>
2017-08-10 11:18:55 +0800

    提现，奖励统计

--- pages/my/withdraw/withdraw.js
+++ pages/my/withdraw/withdraw.js
@@ -1,6 +1,6 @@
 // withdraw.js
 
-let {Tool, RequestReadFactory, Event} = global;
+let {Tool, Storage, RequestReadFactory, RequestWriteFactory} = global;
 
 Page({
 
@@ -8,28 +8,29 @@ Page({
      * 页面的初始数据
      */
     data: {
-        datas: [
-            {
-                'inputName': 'name',
-                'title': '支付宝用户名:',
-                'placeholder': '姓名',
-                'value': ''
-            },
-            {
-                'inputName': 'account',
-                'title': '对方账户:',
-                'placeholder': '支付宝账号／手机号码',
-                'value': ''
-            }
-        ],
+        balance: '',
+        havePassword:false,
+        showInputAlert:false,
+        placeholder:'请输入支付密码',
+        password:'',
+        amount:''
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        this.requestData();
-        Event.on('addAlipayAccountFinish', this.requestData, this)
+        let self = this;
+        wx.getStorage({
+            key: 'memberInfo',
+            success: function (res) {
+                let memberInfo = res.data;
+                self.setData({
+                    balance: memberInfo.Balance,
+                    havePassword: memberInfo.HavePassword
+                });
+            },
+        })
     },
 
     /**
@@ -57,7 +58,7 @@ Page({
      * 生命周期函数--监听页面卸载
      */
     onUnload: function () {
-        Event.off('addAlipayAccountFinish', this.requestData)
+
     },
 
     /**
@@ -82,42 +83,120 @@ Page({
     },
 
     /**
-     * 绑定支付宝 
+     * 提现明细 
      */
-    bindAlipayTap: function () {
-        wx.redirectTo({
-            url: '../withdraw/bind-alipay/bind-alipay',
+    withdrawDetailTap: function () {
+        wx.navigateTo({
+            url: '../withdraw-detail/withdraw-detail',
         })
     },
 
     /**
-     * 下一步
+     * 确认提现 
      */
     formSubmit: function (e) {
         let value = e.detail.value;
-        let name = value.name;
-        let account = value.account;
+        let amount = value.amount;
 
-        wx.redirectTo({
-            url: '../withdraw/withdraw-to-alipay/withdraw-to-alipay?name=' + name + '&account='+ account,
-        })
+        if (amount > this.data.balance) {
+            wx.showModal({
+                title: '提示',
+                content: '超过最大提现金额！',
+                showCancel:false,
+            })
+            return;
+        }
+
+        if (amount < 100) {
+            wx.showModal({
+                title: '提示',
+                content: '提现金额不能低于100',
+                showCancel: false,
+            })
+            return;
+        }
+
+        this.setData({
+            amount: amount
+        });
+
+        if (this.data.havePassword){
+
+            this.setData({
+                showInputAlert: true,
+            })
+
+        }else{
+            wx.showModal({
+                title: '提示',
+                content: '尚未设置提现密码',
+                confirmText:'去设置',
+                success: function (res) {
+                    if (res.confirm) {
+                        wx.navigateTo({
+                            url: '../complete-info/complete-info',
+                        })
+                    }
+                }
+            })
+        }
+    },
+
+    dismissInputAlert:function() {
+        this.setData({
+            showInputAlert: false//隐藏
+        });
+    },
+
+    alertInputOnChange: function(e) {
+        let text = e.detail.value;
+        this.setData({
+            password: text
+        });
+    },
+
+    inputAlertBtnClicked: function(e) {
+        let index = e.currentTarget.dataset.index;
+        this.dismissInputAlert();
+
+        if(index == 1){//确定
+            let r = RequestWriteFactory.withdrawAddRequest(this.data.amount, this.data.password);
+            r.finishBlock = (req) => {
+                wx.showToast({
+                    title: '操作成功！',
+                })
+
+                this.setData({
+                    amount: '',
+                    
+                });
+
+                this.requestMemberInfo();
+            };
+            r.addToQueue();
+        }
     },
 
     /**
-     * 绑定支付宝账号查询
-     */
-    requestData: function () {
-        let r = RequestReadFactory.alipyAccountRead();
+    * 登录用户信息 
+    */
+    requestMemberInfo: function () {
+        let r = RequestReadFactory.memberInfoRead();
         r.finishBlock = (req) => {
             let datas = req.responseObject.Datas;
             datas.forEach((item, index) => {
+
+                wx.setStorage({
+                    key: 'memberInfo',
+                    data: item,
+                })
+
                 this.setData({
-                    'datas[0].value': item.Name,
-                    'datas[1].value': item.AplipayAccount
+                    balance: item.Balance,
                 });
 
             });
         };
         r.addToQueue();
-    },
+    }
 })
