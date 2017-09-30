baa1699d30d843aef02a9e0b86a1d02d1db29fff
zjn <zhangjunneng>
2017-09-30 15:00:06 +0800

    no message

--- pages/find/find.js
+++ pages/find/find.js
@@ -1,96 +1,98 @@
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        listData: [
-            {
-                image: "/res/img/find/find-turnable-icon.png",
-                name: "天天转盘",
-                arrowHidden: true,
-                isShow: false,
-            },
-            {
-                image: "/res/img/find/find-seckill-icon.png",
-                name: "TOP秒杀",
-                arrowHidden: true,
-                isShow: false,
-            },
-            {
-                image: "/res/img/find/find-crowd-icon.png",
-                name: "TOP众筹",
-                arrowHidden: true,
-                isShow: false,
-            },
-            {
-                image: "/res/img/find/find-top-buy-icon.png",
-                name: "TOP团购",
-                arrowHidden: true,
-                isShow: false,
-            },
-            {
-                image: "/res/img/find/find-levy-icon.png",
-                name: "黄金便征集令",
-                arrowHidden: true,
-                isShow: true,
-            },
-            {
-                image: "/res/img/find/find-tool-icon.png",
-                name: "育儿工具",
-                arrowHidden: true,
-                isShow: false,
-            },
-            {
-                image: "/res/img/find/find-knowledge-icon.png",
-                name: "知识库",
-                arrowHidden: true,
-                isShow: false,
-            },
-        ],
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menuArray: []
+  },
 
-    /**
-     * 便便诊所
-     */
-    goClinic: function () {
-        wx.navigateTo({
-            url: '../find/clinic/clinic?type=0',
-        })
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.initMenuArray();
+  },
 
-    /**
-     * 爱牙卫士
-     */
-    goTooth: function () {
-        wx.navigateTo({
-            url: '../find/clinic/clinic?type=1',
-        })
-    },
+  /**
+   * 进入便便诊所和爱牙卫士
+   */
+  onTopListener: function (e) {
+    let position = e.currentTarget.dataset.position;
+    wx.navigateTo({
+      url: '../find/clinic/clinic?position=' + position,
+    })
+  },
 
-    /**
-     * 点击进入详情
-     */
-    goDetail: function (e) {
-        let position = e.currentTarget.dataset.index;
-        console.log("=======" + position);
+  /**
+   * 点击进入详情
+   */
+  onMenuItemListener: function (e) {
+    let position = e.currentTarget.dataset.index;
+    console.log("=======" + position);
 
-        if (position == 0) {
-            wx.navigateTo({
-                url: '../find/lottery/lottery',
-            })
-        } else if (position == 1) {
-        } else if (position == 2) {
-        } else if (position == 3) {
-        } else if (position == 4) {
-            wx.navigateTo({
-                url: '../find/levy/levy',
-            })
-        } else if (position == 5) {
-            wx.navigateTo({
-                url: '../find/tool/tool',
-            })
-        } else if (position == 6) {
-        }
+    if (position == 0) {
+      wx.navigateTo({
+        url: '../find/lottery/lottery',
+      })
+    } else if (position == 1) {
+    } else if (position == 2) {
+    } else if (position == 3) {
+    } else if (position == 4) {
+      wx.navigateTo({
+        url: '../find/levy/levy',
+      })
+    } else if (position == 5) {
+      wx.navigateTo({
+        url: '../find/tool/tool',
+      })
+    } else if (position == 6) {
     }
+  },
+  /**
+   * 初始化菜单数据
+   */
+  initMenuArray: function () {
+    let menuArray = [
+      {
+        icon: "/res/img/find/find-turnable-icon.png",
+        title: "天天转盘",
+        hasArrow: true
+      },
+      {
+        icon: "/res/img/find/find-seckill-icon.png",
+        title: "TOP秒杀",
+        hasArrow: true
+      },
+      {
+        icon: "/res/img/find/find-crowd-icon.png",
+        title: "TOP众筹",
+        hasArrow: true
+      },
+      {
+        icon: "/res/img/find/find-top-buy-icon.png",
+        title: "TOP团购",
+        hasArrow: true
+      },
+      {
+        icon: "/res/img/find/find-levy-icon.png",
+        title: "黄金便征集令",
+        hasArrow: true
+      },
+      {
+        icon: "/res/img/find/find-tool-icon.png",
+        title: "育儿工具",
+        hasArrow: true,
+        hasDivide: true
+      },
+      {
+        icon: "/res/img/find/find-knowledge-icon.png",
+        title: "知识库",
+        hasArrow: true
+      }
+    ];
+    this.setData({
+      menuArray: menuArray
+    });
+  }
 })
--- pages/my/my.js
+++ pages/my/my.js
@@ -3,283 +3,304 @@
 let { Tool, Storage, RequestReadFactory } = global;
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        nickName: '',
-        avatarUrl: '/res/img/common/common-avatar-default-icon.png',
-        sign: '',
-        shopName: '',
-        idDesp: '',
-        inviteCode: '',
-        points: '12367',
-        coins: '500',
-        fav: '10',
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menuArray: [],
+    nickName: '',
+    avatarUrl: '/res/img/common/common-avatar-default-icon.png',
+    sign: '',
+    shopName: '',
+    idDesp: '',
+    inviteCode: '',
+    points: '12367',
+    coins: '500',
+    fav: '10'
+  },
 
-        myDatasItems0: [
-            {
-                image: '/res/img/my/cell/my-cell-award-icon.png',
-                name: '我的奖励',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-sign-icon.png',
-                name: '赚金币',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-baby-diary-icon.png',
-                name: '宝宝日记',
-                isShow: true
-            },
-            {
-                image: '/res/img/my/cell/my-cell-seckill-icon.png',
-                name: '我的秒杀',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-raise-icon.png',
-                name: '我的众筹',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-group-buy-icon.png',
-                name: '我的团购',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-free-trial-icon.png',
-                name: '我的试用',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-points-order-icon.png',
-                name: '我的积分订单',
-                isShow: true
-            },
-            {
-                image: '/res/img/my/cell/my-cell-qa-icon.png',
-                name: '我的问答',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-create-post-icon.png',
-                name: '发表的帖子',
-            },
-            {
-                image: '/res/img/my/cell/my-cell-reply-post-icon.png',
-                name: '回复的帖子',
-                isShow: true
-            },
-            {
-                image: '/res/img/my/cell/my-cell-address-icon.png',
-                name: '收货地址管理',
-            },
-        ]
-    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.initMenuArray();
+    this.requestData();
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        this.requestData();
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+  },
 
-    },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+  },
 
-    },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+  },
 
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
+  },
 
-    },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+  },
 
-    },
+  coinsTap: function () {
+    wx.navigateTo({
+      url: '../my/coins/coins',
+    })
+  },
 
-    coinsTap: function () {
-        wx.navigateTo({
-            url: '../my/coins/coins',
-        })
-    },
+  favTap: function () {
+    wx.navigateTo({
+      url: '../my/my-fav/my-fav',
+    })
+  },
 
-    favTap: function () {
-        wx.navigateTo({
-            url: '../my/my-fav/my-fav',
-        })
-    },
+  /**
+   * 菜单点击
+   */
+  onMenuItemListener: function (e) {
+    let title = e.currentTarget.dataset.title;
+    console.log('--------' + title);
+    if (title == '我的奖励') {
+      wx.navigateTo({
+        url: '../my/my-award/my-award',
+      })
 
-    /**
-     * cell点击
-     */
-    goDetail: function (e) {
-        let title = e.currentTarget.dataset.title;
-        console.log('--------' + title);
-        if (title == '我的奖励') {
-            wx.navigateTo({
-                url: '../my/my-award/my-award',
-            })
+    } else if (title == '赚金币') {
+      wx.navigateTo({
+        url: '../my/sign/sign',
+      })
 
-        } else if (title == '赚金币') {
-            wx.navigateTo({
-                url: '../my/sign/sign',
-            })
+    } else if (title == '宝宝日记') {
+      wx.navigateTo({
+        url: '../my/baby-diary/baby-diary',
+      })
 
-        } else if (title == '宝宝日记') {
-            wx.navigateTo({
-                url: '../my/baby-diary/baby-diary',
-            })
+    } else if (title == '我的秒杀') {
 
-        } else if (title == '我的秒杀') {
+    } else if (title == '我的众筹') {
 
-        } else if (title == '我的众筹') {
+    } else if (title == '我的团购') {
 
-        } else if (title == '我的团购') {
+    } else if (title == '我的试用') {
 
-        } else if (title == '我的试用') {
+    } else if (title == '我的积分订单') {
+      wx.navigateTo({
+        url: '../my/myOrder/myOrder',
+      })
 
-        } else if (title == '我的积分订单') {
-            wx.navigateTo({
-                url: '../my/myOrder/myOrder',
-            })
+    } else if (title == '我的问答') {
+      wx.navigateTo({
+        url: '../my/my-question/my-question',
+      })
 
-        } else if (title == '我的问答') {
-            wx.navigateTo({
-                url: '../my/my-question/my-question',
-            })
+    } else if (title == '发表的帖子') {
+      wx.navigateTo({
+        url: '../my/my-create-post/my-create-post',
+      })
 
-        } else if (title == '发表的帖子') {
-            wx.navigateTo({
-                url: '../my/my-create-post/my-create-post',
-            })
+    } else if (title == '回复的帖子') {
+      wx.navigateTo({
+        url: '../my/my-reply-post/my-reply-post',
+      })
 
-        } else if (title == '回复的帖子') {
-            wx.navigateTo({
-                url: '../my/my-reply-post/my-reply-post',
-            })
+    } else if (title == '收货地址管理') {
+      wx.navigateTo({
+        url: '../address/address',
+      })
+    }
+  },
 
-        } else if (title == '收货地址管理') {
-            wx.navigateTo({
-                url: '../address/address',
-            })
+  /**
+   * 消息
+   */
+  messageTap: function () {
+    console.log('----消息----');
+  },
+
+  /**
+   * 设置
+   */
+  settingTap: function () {
+    console.log('----设置----');
+
+    wx.navigateTo({
+      url: '../my/setting/setting',
+    })
+  },
+
+  /**
+   * 编辑资料
+   */
+  editProfileTap: function () {
+    console.log('----编辑资料----');
+
+    wx.navigateTo({
+      url: '../my/edit-profile/edit-profile',
+    })
+  },
+
+  /**
+   * 请求统一入口
+   */
+  requestData: function () {
+    this.requestMemberInfo();
+  },
+
+  /**
+   * 登录用户信息 
+   */
+  requestMemberInfo: function () {
+    let r = RequestReadFactory.memberInfoRead();
+    r.finishBlock = (req) => {
+      let datas = req.responseObject.Datas;
+      datas.forEach((item, index) => {
+
+        //是否为内部员工
+        Storage.setInsideMember(item.Inside);
+
+        //头像url
+        let url = Tool.imageURLForId(item.PictureId);
+
+        //身份描述设置
+        let name = '';
+        let desp = '';
+        if (item.MemberTypeKey == 0) {//普通员工
+          name = item.Nickname;
+        } else if (item.MemberTypeKey == 1 || item.MemberTypeKey == 3) {//内部员工和门店
+          name = item.ShopName;
+          desp = '零售合伙人';
+        } else if (item.MemberTypeKey == 2) {//经销商
+          name = item.ShopName;
+          desp = '城市合伙人';
+
         }
-    },
 
-    /**
-     * 消息
-     */
-    messageTap: function () {
-        console.log('----消息----');
-    },
+        this.setData({
+          nickName: item.Nickname,
+          sign: item.Sign,
+          avatarUrl: url,
+          shopName: name,
+          idDesp: desp,
+          inviteCode: item.InvitationCode,
+          'myDatasItems0[1].detail.amount': item.Balance,
+          'myDatasItems0[2].detail.amount': item.Commission,
+          'myDatasItems0[3].detail.amount': item.BuyerCommission,
+          'myDatasItems0[4].detail.amount': item.PartnerCommission,
+          'myDatasItems1[0].detail.amount': item.FirstFriend,
+          'myDatasItems1[1].detail.amount': item.ShopPersonCount,
+          'myDatasItems1[2].detail.amount': item.SecondFriends,
+        });
 
-    /**
-     * 设置
-     */
-    settingTap: function () {
-        console.log('----设置----');
-
-        wx.navigateTo({
-            url: '../my/setting/setting',
-        })
-    },
-
-    /**
-     * 编辑资料
-     */
-    editProfileTap: function () {
-        console.log('----编辑资料----');
-
-        wx.navigateTo({
-            url: '../my/edit-profile/edit-profile',
-        })
-    },
-
-    /**
-     * 请求统一入口
-     */
-    requestData: function () {
-        this.requestMemberInfo();
-    },
-
-    /**
-     * 登录用户信息 
-     */
-    requestMemberInfo: function () {
-        let r = RequestReadFactory.memberInfoRead();
-        r.finishBlock = (req) => {
-            let datas = req.responseObject.Datas;
-            datas.forEach((item, index) => {
-
-                //是否为内部员工
-                Storage.setInsideMember(item.Inside);
-
-                //头像url
-                let url = Tool.imageURLForId(item.PictureId);
-
-                //身份描述设置
-                let name = '';
-                let desp = '';
-                if (item.MemberTypeKey == 0) {//普通员工
-                    name = item.Nickname;
-                } else if (item.MemberTypeKey == 1 || item.MemberTypeKey == 3) {//内部员工和门店
-                    name = item.ShopName;
-                    desp = '零售合伙人';
-                } else if (item.MemberTypeKey == 2) {//经销商
-                    name = item.ShopName;
-                    desp = '城市合伙人';
-
-                }
-
-                this.setData({
-                    nickName: item.Nickname,
-                    sign: item.Sign,
-                    avatarUrl: url,
-                    shopName: name,
-                    idDesp: desp,
-                    inviteCode: item.InvitationCode,
-                    'myDatasItems0[1].detail.amount': item.Balance,
-                    'myDatasItems0[2].detail.amount': item.Commission,
-                    'myDatasItems0[3].detail.amount': item.BuyerCommission,
-                    'myDatasItems0[4].detail.amount': item.PartnerCommission,
-                    'myDatasItems1[0].detail.amount': item.FirstFriend,
-                    'myDatasItems1[1].detail.amount': item.ShopPersonCount,
-                    'myDatasItems1[2].detail.amount': item.SecondFriends,
-                });
-
-            });
-        };
-        r.addToQueue();
-    },
+      });
+    };
+    r.addToQueue();
+  },
+  /**
+ * 初始化菜单数据
+ */
+  initMenuArray: function () {
+    let menuArray = [
+      {
+        icon: '/res/img/my/cell/my-cell-award-icon.png',
+        title: '我的奖励',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-sign-icon.png',
+        title: '赚金币',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-baby-diary-icon.png',
+        title: '宝宝日记',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-seckill-icon.png',
+        title: '我的秒杀',
+        hasArrow: true,
+        hasDivide: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-raise-icon.png',
+        title: '我的众筹',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-group-buy-icon.png',
+        title: '我的团购',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-free-trial-icon.png',
+        title: '我的试用',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-points-order-icon.png',
+        title: '我的积分订单',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-qa-icon.png',
+        title: '我的问答',
+        hasArrow: true,
+        hasDivide: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-create-post-icon.png',
+        title: '发表的帖子',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-reply-post-icon.png',
+        title: '回复的帖子',
+        hasArrow: true
+      },
+      {
+        icon: '/res/img/my/cell/my-cell-address-icon.png',
+        title: '收货地址管理',
+        hasArrow: true,
+        hasDivide: true
+      }
+    ];
+    this.setData({
+      menuArray: menuArray
+    });
+  }
 })
